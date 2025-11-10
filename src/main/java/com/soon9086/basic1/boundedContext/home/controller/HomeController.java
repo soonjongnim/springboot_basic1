97b06d6366b9d2d9bd525eb07f37e6e1767d9ffa
soonjongnim <soonjongnim@naver.com>
2025-11-10 17:08:38 +0900

    person add, show, del 기능구현

--- src/main/java/com/soon9086/basic1/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/soon9086/basic1/boundedContext/home/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.soon9086.basic1.boundedContext.home.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +19,12 @@ import java.util.*;
 //     - 사용자의 요청(URL)을 받아서 처리하고, 응답을 반환함.
 @Controller
 public class HomeController {
-    int num;
+    private int num;
+    private List<Person> personList;
 
     public HomeController() {
         num = 0;
+        personList = new ArrayList<>();
     }
     // ② @GetMapping("/home/main")
     //     - 사용자가 "/home/main" 주소로 GET 방식 요청을 보낼 때,
@@ -111,5 +116,114 @@ public class HomeController {
         }};
         return map;
     }
+    @GetMapping("/home/returnArticle")
+    @ResponseBody
+    public Article2 showReturnArticle() {
+        Article2 article2 = new Article2(1, "제목1", "내용1", "김철수", new ArrayList<>() {{
+            add(1);
+            add(2);
+            add(3);
+        }});
+        return article2;
+    }
+    @GetMapping("/home/returnArticleList")
+    @ResponseBody
+    public List<Article2> showReturnArticleList() {
+        Article2 article1 = new Article2(1, "제목1", "내용1", "김철수", new ArrayList<>() {{
+            add(1);
+            add(2);
+            add(3);
+        }});
+        Article2 article2 = new Article2(2, "제목2", "내용2", "김영희", new ArrayList<>() {{
+            add(4);
+            add(5);
+            add(6);
+        }});
+        Article2 article3 = new Article2(3, "제목3", "내용4", "최수영", new ArrayList<>() {{
+            add(7);
+            add(8);
+            add(9);
+        }});
+
+        List<Article2> list = new ArrayList<>();
+        list.add(article1);
+        list.add(article2);
+        list.add(article3);
+        return list;
+    }
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age) {
+        Person person = new Person(name, age);
+        System.out.println("person: " + person);
+        personList.add(person);
+        System.out.println("personList: " + personList);
+        return "name : %s, age : %d".formatted(name, age);
+    }
+    @GetMapping("/home/showPerson")
+    @ResponseBody
+    public List<Person> showPerson() {
+        return personList;
+    }
+    @GetMapping("/home/removePerson")
+    @ResponseBody
+    public String removePerson(int id) {
+//        Person target = null;
+//        for (Person person : personList) {
+//            if (person.getId() == id) {
+//                target = person;
+//                break;
+//            }
+//        }
+//        if  (target != null) {
+//            personList.remove(target);
+//        } else {
+//            return "%d번 사람이 존재하지 않습니다.".formatted(id);
+//        }
+        // V2
+        boolean removed = personList.removeIf(person -> person.getId() == id);
+        if (!removed) {
+            return "%d번 사람이 존재하지 않습니다.".formatted(id);
+        }
+        return "%d번 삭제 처리하였습니다.".formatted(id);
+    }
+
+}
+
+class Article {
+    public int id;
+    public String subject;
+    public String content;
+    public String writerName;
+    public List<Integer> articleNo;
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class Article2 {
+    private int id;
+    private String subject;
+    private String content;
+    private String writerName;
+    private List<Integer> articleNo;
+}
+
+@AllArgsConstructor
+@Data
+class Person {
+    private static int lastId;
+    private final int id;
+    private final String name;
+    private final int age;
+
+    static {
+        lastId = 0;
+    }
+
+    public Person(String name, int age) {
+        this(++lastId, name, age);
+    }
+
 }
 
