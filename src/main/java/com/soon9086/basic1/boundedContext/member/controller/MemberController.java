38fa1d87e4f8fa75b5a178909336258f6486c770
soonjongnim <soonjongnim@naver.com>
2025-11-11 15:29:06 +0900

    Rq 객체 생성을 스프링 컨테이너에게 맡김

--- src/main/java/com/soon9086/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/soon9086/basic1/boundedContext/member/controller/MemberController.java
@@ -7,24 +7,29 @@ import com.soon9086.basic1.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 
+@AllArgsConstructor // 자동 생성자 주입
 @Controller
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
+    // 생성자 주입
+    /*
     public MemberController() {
         memberService = new MemberService();
     }
+    */
 
     @GetMapping("/member/login")
     @ResponseBody
     public RsData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
         if(username.trim().isEmpty()) {
             return RsData.of("F-1", "아이디를 입력해주세요.");
         } else if(password.trim().isEmpty()) {
@@ -43,7 +48,6 @@ public class MemberController {
     @GetMapping("/member/logout")
     @ResponseBody
     public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
         boolean cookieRemoved = rq.removeCookie("loginedMemberId");
         if(!cookieRemoved) {
             return RsData.of("F-1", "이미 로그아웃 상태입니다.");
@@ -55,7 +59,6 @@ public class MemberController {
     @GetMapping("/member/me")
     @ResponseBody
     public RsData showMe(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
         long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);   // 0은 로그인이 안되어 있다는 의미
         boolean isLogined = loginedMemberId > 0;
         if(!isLogined) {
